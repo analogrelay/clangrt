f95451c182a34e1f483ea43f4805294a5382878e
anurse <andrew@andrewnurse.net>
2012-12-02 23:51:33 -0800

    Initial Import

---
+++ ClangRT/Index.cpp
@@ -0,0 +1,8 @@
+#include "Index.h"
+#include <Index.h>
+
+using namespace ClangRT;
+
+Index^ Index::Open() {
+	
+}
---
+++ ClangRT/Index.h
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace ClangRT {
+	public ref class Index sealed {
+	public:
+		static Index^ Open();
+	};
+}
